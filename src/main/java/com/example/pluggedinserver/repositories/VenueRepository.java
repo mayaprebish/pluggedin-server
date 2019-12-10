ffb191ac48f82c671744e27e496bebda48604ae7
molambo <lambo.m@husky.neu.edu>
2019-12-10 14:12:16 -0500

    Added a find venue by id query to venue repository

--- src/main/java/com/example/pluggedinserver/repositories/VenueRepository.java
+++ src/main/java/com/example/pluggedinserver/repositories/VenueRepository.java
@@ -1,8 +1,13 @@
 package com.example.pluggedinserver.repositories;
 
 import com.example.pluggedinserver.models.Venue;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface VenueRepository extends CrudRepository<Venue, Integer> {
+
+    @Query("select venue from Venue venue where venue.id=:vid")
+    public Venue findVenueById(@Param("vid") Integer id);
 
 }
