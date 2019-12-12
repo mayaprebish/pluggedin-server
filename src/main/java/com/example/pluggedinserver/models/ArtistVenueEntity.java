c877fa81aa926e2ec239b84d1473fada89484348
Maya Prebish <prebish.m@husky.neu.edu>
2019-12-12 15:47:12 -0500

    latest

--- src/main/java/com/example/pluggedinserver/models/ArtistVenueEntity.java
+++ src/main/java/com/example/pluggedinserver/models/ArtistVenueEntity.java
@@ -17,4 +17,29 @@ class ArtistVenueEntity {
     @JoinColumn(name = "venue_id")
     Venue venue;
 
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Artist getArtist() {
+        return artist;
+    }
+
+    public void setArtist(Artist artist) {
+        this.artist = artist;
+    }
+
+    public Venue getVenue() {
+        return venue;
+    }
+
+    public void setVenue(Venue venue) {
+        this.venue = venue;
+    }
+
+    public ArtistVenueEntity() {}
 }
